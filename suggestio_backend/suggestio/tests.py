75e91dab99d39cafecd93d44953bebb21cffaf2b
Павел <78384513+piligrimdev@users.noreply.github.com>
2024-11-28 23:56:34 +0300

    Added linter and unit tests in gitlab-ci

--- suggestio_backend/suggestio/tests.py
+++ suggestio_backend/suggestio/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SampleTest(TestCase):
+    def setUp(self) -> None:
+        print('setUp')
+
+    def tearDown(self) -> None:
+        print('tearDown')
+
+    def test_sample(self):
+        self.assertTrue(1 == 1)
